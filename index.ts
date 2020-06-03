8c344e63f3bebc3050db4d2c90740e1c602df0ad
Steve Jabour <steve@jabour.me>
2020-06-03 14:37:07 -0600

    Export exceptions to make life easier

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ export * from './lib/application-package';
 export * from './lib/application-render-readme';
 export * from './lib/application';
 export * from './lib/create-application';
+export * from './lib/exceptions';
 export * from './lib/logger';
 export * from './lib/params';
